1f15e62fc9ff176ee428f04e1917d256f6b2f485
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-04 14:37:45 -0400

    Initial Commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,20 @@
+import type { LinksFunction } from "@remix-run/node";
+
 import {
   Form,
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
   return (
@@ -45,7 +54,9 @@ export default function App() {
             </ul>
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
